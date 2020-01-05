39d37937423f847c228abd40da7fc24c93e39a9d
shaolun Ruan <haywardryan@foxmail.com>
2020-01-05 17:03:02 +0800

    构建vtkjs项目

---
+++ node_modules/vtk.js/Sources/Widgets/Core/WidgetManager/Constants.js
@@ -0,0 +1,22 @@
+export const ViewTypes = {
+  DEFAULT: 0,
+  GEOMETRY: 1,
+  SLICE: 2,
+  VOLUME: 3,
+};
+
+export const RenderingTypes = {
+  PICKING_BUFFER: 0,
+  FRONT_BUFFER: 1,
+};
+
+export const CaptureOn = {
+  MOUSE_MOVE: 0,
+  MOUSE_RELEASE: 1,
+};
+
+export default {
+  ViewTypes,
+  RenderingTypes,
+  CaptureOn,
+};
